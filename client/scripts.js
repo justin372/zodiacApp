04fc414d01d28ea0fc22effc2d5ff97b96827f08
justin372 <70275559+justin372@users.noreply.github.com>
2020-11-26 03:24:08 -0800

    few changes made

---
+++ client/scripts.js
@@ -0,0 +1,60 @@
+var zodiacs = [
+{
+	name: "Aries",
+	month: "March 21-april 20",
+	description: "ruled by marse. energy and life. other stuff too. not real website soooo bla bal computer stuff",
+    
+ },
+ {
+ 	name:"Taurus",
+ 	month: "Aprile 20 to may 20",
+ 	description: "does something. i dont know google........ it you dumb ass.",
+ },
+ {
+ 	name:"Gemini",
+ 	month:"may 21 june 20",
+ 	description"aaaaaaaaa i i i  i computer computers doess stuffffffffffffffff"
+ },
+ {
+ name:"cancer",
+ month:"i really dont care to google this stuff you got this",
+ description:"killed your mother hay yooooooo.",
+ },
+ {
+ 	name:"Leo",
+ 	month:"july something",
+ 	description:"bad all. lives under the big apple. likes to fight crime",
+ },
+
+ {
+ 	name:"Virgo",
+ 	month:"something",
+ 	description:"badgsdfgdsfgsdfgsdfgdsfgsdfgds fgsdfgsdfgs dfgdsfg dsfgsdfg",
+ },
+
+ {
+ 	name:"Libra",
+ 	month:"GOOOOGGGLLLLEEE IIITTTT",
+ 	description:"do you ever feel like a plastic bag floating through the wind feeling so paper thin",
+ },
+ {
+ 	name:"Scorpio",
+ 	month:"*wispers in ear * im behing you",
+ 	description:"badle. likes to fight crime",
+ },
+ {
+ 	name:"Sagittarius",
+ 	month:"i think november or december",
+ 	description:"bad all. lives under the big apple. likes to fight crime",
+ },
+  {
+ 	name:"Capricorn",
+ 	month:"wow i really have no life",
+ 	description:"Hes a big bad capricorn ready to f## you up",
+ },
+  {
+ 	name:"I give up theres too many",
+ 	month:"aaaaaaaaaaaaasssssssss bbbeeeee ffiiinnee",
+ 	description:"crabs be crabs you ho",
+ },
+]
